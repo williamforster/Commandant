df3af661e8a5973d2e1dbc3c342e6a45383463cb
William Forster <williamforster@Williams-MacBook-Air.local>
2019-07-25 11:09:17 +1000

    Change styles and add logout button

--- index.php
+++ index.php
@@ -4,11 +4,15 @@
     <meta charset='utf-8'>
     <title>Vehicle Tracker</title>
     <link rel="stylesheet" href="https://openlayers.org/en/v5.3.0/css/ol.css" type="text/css">
+    <link rel="stylesheet" href="style/main.css" type="text/css">
     <link rel="stylesheet" href="style/map.css" type="text/css">
     <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=requestAnimationFrame,Element.prototype.classList,URL"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
   </head>
   <body>
+    <div id="topbar">
+      <a href="#"><img src="img/user-icon.png" alt="logout icon"/>logout</a>
+    </div><!--topbar-->
     <div id="map" class="map"></div>
     <div id="map-datapoint-popup">
       <div id="map-datapoint-popup-content"></div>
